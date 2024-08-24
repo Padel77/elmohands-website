12b6f4cfe95115ebca13db9843f989f21dee0a39
Padel77 <MY_NAME@example.co>
2024-08-25 00:58:42 +0300

    update  unless component

--- src/app/[locale]/(website)/services/loading.tsx
+++
@@ -1,6 +0,0 @@
-import LoadingPage from "@/components/loading/loadingPage";
-
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <LoadingPage />
-  }
